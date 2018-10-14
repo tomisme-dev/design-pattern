0a7761935bd47335e12b0d65ea3ecaad48ad7aed
yo <yo@DESKTOP-BBSI5JJ>
2018-10-14 17:27:43 +0800

    提交

--- src/main/java/com/youaiduan/simplefactory/Demo1.java
+++ src/main/java/com/youaiduan/simplefactory/Demo1.java
@@ -2,6 +2,6 @@ package com.youaiduan.simplefactory;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("1hello world");
 	}
 }
